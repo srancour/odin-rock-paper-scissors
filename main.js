bb83338e4a21ef3765a2296004a793e8f435c9d8
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-17 18:21:02 -0500

    feat: add human choice pseudocode

--- main.js
+++ main.js
@@ -6,17 +6,38 @@ function getComputerChoice() {
     // COMPUTE randomly 1, 2, or 3 for computer's choice
     // SET variable to computer's choice
     let computerChoice = Math.floor(Math.random() * 3) + 1;
+    // log(computerChoice);
+    // IF choice is 1 THEN
+    if (computerChoice === 1){
+        //  SET variable to "rock"
+        computerChoice = "rock";
+    } // ELSE IF choice is 2 THEN
+    else if (computerChoice === 2){
+        //  SET variable to "paper"
+        computerChoice = "paper";
+    } // ELSE IF choice is 3 THEN
+    else if (computerChoice === 3){
+        //  SET variable to "scissors"
+        computerChoice = "scissors";
+    } // ELSE
+    else {
+        //  PRINT warning
+        console.warn("Number out of bounds");
+    } // ENDIF
     log(computerChoice);
-// IF choice is 1 THEN
-//  SET variable to "rock"
-// ELSE IF choice is 2 THEN
-//  SET variable to "paper"
-// ELSE IF choice is 3 THEN
-//  SET variable to "scissors"
-// ELSE
-//  PRINT warning
-// ENDIF
 }
 // CALL computer choice function
 getComputerChoice();
 // END Computer decides on choice
+
+// START Human decides on choice
+// DISPLAY "Rock, paper, or scissors?"
+// GET Human choice
+// IF Human choice is rock THEN
+//  SET variable to "rock"
+// ELSE IF Human choice is paper THEN
+//  SET variable to "paper"
+// ELSE IF Human choice is scissors THEN
+//  SET variable to "scissors"
+// ELSE
+//  DISPLAY "Not a valid choice."
