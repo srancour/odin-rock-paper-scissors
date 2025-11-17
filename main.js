9a5f464491e90b5b1fc6f3f1944d0c2b4524f23e
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-17 18:04:08 -0500

    feat: pseudocode for the computer choice

---
+++ main.js
@@ -0,0 +1,16 @@
+const log = console.log;
+// log("Hello World");
+
+// START Computer decides on choice
+// COMPUTE randomly 1, 2, or 3 for computer's choice
+// SET variable to computer's choice
+// IF choice is 1 THEN
+//  SET variable to "rock"
+// ELSE IF choice is 2 THEN
+//  SET variable to "paper"
+// ELSE IF choice is 3 THEN
+//  SET variable to "scissors"
+// ELSE
+//  PRINT warning
+// ENDIF
+// END Computer decides on choice
